ed396e737891ba588b5a43a0eb0d81257304b248
Matheus Neves <matheusneves1989@gmail.com>
2021-05-31 19:24:49 -0300

    Hello World!

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Flex } from "@chakra-ui/layout"
+import Head from "next/head"
+
+const Home = () => {
+  return (
+    <Flex>
+      <Head>
+        <title>Worldtrip | Home</title>
+      </Head>
+      <h1>Hello World!</h1>
+    </Flex>
+  )
+}
+
+export default Home
